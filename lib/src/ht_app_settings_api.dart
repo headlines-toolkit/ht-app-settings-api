108ac1c5201fc0b4cc3e587a7073872291de2ecd
fulleni <fulleni@hotmail.com>
2025-04-30 07:11:34 +0100

    chore(ht_app_settings_api): create initial project structure
    - Add basic project files and configuration for a Dart package
    - Include Very Good CLI generated templates and workflows
    - Set up CI/CD with GitHub Actions
    - Add dependencies and initial test

---
+++ lib/src/ht_app_settings_api.dart
@@ -0,0 +1,7 @@
+/// {@template ht_app_settings_api}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class HtAppSettingsApi {
+  /// {@macro ht_app_settings_api}
+  const HtAppSettingsApi();
+}
---
+++ test/src/ht_app_settings_api_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:ht_app_settings_api/ht_app_settings_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('HtAppSettingsApi', () {
+    test('can be instantiated', () {
+      expect(HtAppSettingsApi(), isNotNull);
+    });
+  });
+}
